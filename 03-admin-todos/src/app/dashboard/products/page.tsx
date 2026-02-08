454813f5f4d9fc0bfabfe311a25afb662deacbc4
franHR11 <franhr1113@gmail.com>
2026-02-08 01:52:20 +0100

    feat: Implement product listing page with `ProductCard` and `Star` components.

--- 03-admin-todos/src/app/dashboard/products/page.tsx
+++ 03-admin-todos/src/app/dashboard/products/page.tsx
@@ -1,10 +1,14 @@
 import { ProductCard } from "@/src/products";
+import { products } from "@/src/products/data/products";
 
 export default function ProductsPage() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-            <h1>Products Page</h1>
-            <ProductCard />
+
+
+            {products.map((product) => (
+                <ProductCard key={product.id} {...product} />
+            ))}
         </div>
     );
 }
